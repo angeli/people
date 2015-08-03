e67f65fe6b37757aa9bd076f99c41e189334c75e
Nikolay Dachin <salieri@abv.bg>
2015-08-03 19:17:04 +0300

    Merge origin/master
    Conflicts:
    	resources/views/index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -54,45 +54,46 @@
 			<!--The Map-->
 			<the-map ng-src="assets/svg/Office.svg" selected-desk="testCtrl.desk_id"></the-map>		
 
-			<div class="user-info" ng-controller="UserInfoCtrl as uiCtrl">
+			<div ng-controller="UserInfoCtrl as uiCtrl">
+				<div class="user-info" ng-if="uiCtrl.desk">
+					<div class="row">
+						<div class="col-md-4"><img src="http://sofwks0385/pic/{{uiCtrl.user_id}}.jpg" class="user-picture" /></div>
 
-				<div class="row">
-					<div class="col-md-4"><img src="http://sofwks0385/pic/{{uiCtrl.user_id}}.jpg" class="user-picture" /></div>
-
-					<div class="col-md-7">
-						<div class="user-name">
-							<div>{{uiCtrl.fname}}</div>
-							<div id="user-lname">{{uiCtrl.lname}}</div>
+						<div class="col-md-7">
+							<div class="user-name">
+								<div>{{uiCtrl.fname}}</div>
+								<div id="user-lname">{{uiCtrl.lname}}</div>
+							</div>
+							<div class="user-position">{{uiCtrl.position}}</div>
 						</div>
-						<div class="user-position">{{uiCtrl.position}}</div>
+						<div class="col-md-1">
+							<div class="lock" ng-if="uiCtrl.admin" ng-click="uiCtrl.releseLock()"><i class="{{uiCtrl.is_locked? 'fa fa-lock': 'fa fa-unlock'}}"></i></div>
+						</div>
 					</div>
-					<div class="col-md-1">
-						<div class="lock" ng-if="uiCtrl.admin" ng-click="uiCtrl.releseLock()"><i class="{{uiCtrl.is_locked? 'fa fa-lock': 'fa fa-unlock'}}"></i></div>
-					</div>
-				</div>
 
-				<div class="row user-office-info">
-					<div class="col-md-6">
-						<div class="user-team">Team: {{uiCtrl.team}}</div>
-						<div class="user-team">Departament: {{uiCtrl.departament}}</div>
+					<div class="row user-office-info">
+						<div class="col-md-7">
+							<div class="user-team">Team: {{uiCtrl.team}}</div>
+							<div class="user-team">Departament: {{uiCtrl.department}}</div>
 
-						<div class="user-team" ng-if="!uiCtrl.edit">Office seat: {{uiCtrl.seat}}</div>
-						<div class="user-team edit" ng-if="uiCtrl.edit">
-							Office seat: <input type="text" ng-model="uiCtrl.seat" size="2">
+							<div class="user-team" ng-if="!uiCtrl.edit">Office desk: {{uiCtrl.desk}}</div>
+							<div class="user-team edit" ng-if="uiCtrl.edit">
+								Office desk: <input type="text" ng-model="uiCtrl.desk" size="2">
+							</div>
+
 						</div>
 
+						<div class="edit-ok col-md-1" ng-if="uiCtrl.edit" ng-click="uiCtrl.chengeDesk()"><i class="fa fa-pencil"></i></div>
+							<div class="col-md-1"></div>
+						<div class="edit-leave col-md-1" ng-if="uiCtrl.edit" ng-click="uiCtrl.leaveDesk()"><i class="fa fa-close"></i></div>
 					</div>
 
-					<div class="edit-ok col-md-1" ng-if="uiCtrl.edit" ng-click="uiCtrl.chengeSeat()"><i class="fa fa-pencil"></i></div>
-						<div class="col-md-1"></div>
-					<div class="edit-leave col-md-1" ng-if="uiCtrl.edit" ng-click="uiCtrl.leaveSeat()"><i class="fa fa-close"></i></div>
+					<div class="row user-mail">
+						<div><a href="mailto:{{uiCtrl.mail}}">{{uiCtrl.mail}}</a></div>
+					</div>
 				</div>
-
-				<div class="row user-mail">
-					<div><a href="mailto:{{uiCtrl.mail}}">{{uiCtrl.mail}}</a></div>
-				</div>
-
 			</div>
+
 			
 						
 			<search ng-if="mainCtrl.isReady()" map="mainCtrl.map"></search>
